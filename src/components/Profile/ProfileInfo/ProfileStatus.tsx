e1877752a0345766ab8a167d9070026507e353dd
ElenaBoyko1992 <Bionka@inbox.ru>
2023-01-11 13:39:23 +0300

    72 lesson done

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -2,17 +2,38 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 
 
-const ProfileStatus = (props: any) => {
-    return (
-        <div>
+class ProfileStatus extends React.Component<any, any> {
+    state = {
+        editMode: false
+    }
+
+    activateEditMode = () => {
+        this.setState({
+            editMode: true
+        })
+    }
+
+    deactivateEditMode = () => {
+        this.setState({
+            editMode: false
+        })
+    }
+
+    render() {
+        return (
             <div>
-                <span>{props.status}</span>
+                {!this.state.editMode &&
+                    <div>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+                    </div>}
+                {this.state.editMode &&
+                    <div>
+                        <input value={this.props.status} onBlur={this.deactivateEditMode} autoFocus={true}/>
+                    </div>}
             </div>
-            <div>
-                <input value={props.status}/>
-            </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 export default ProfileStatus;
