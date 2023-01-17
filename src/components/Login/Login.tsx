35bfae1e2f3057fe92a74765b3915f2cd2be94df
ElenaBoyko1992 <Bionka@inbox.ru>
2023-01-17 19:10:29 +0300

    75 lesson done + typing

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
+
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
 
 const Login = () => {
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
+
     return (
-        <h1>
-            LOGIN
-        </h1>
+        <div>
+            <h1>
+                Login
+            </h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
     );
 };
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+        return (
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <Field placeholder={'Login'} component={'input'} name={'login'}/>
+                </div>
+                <div>
+                    <Field placeholder={'Password'} component={'input'} name={'password'}/>
+                </div>
+                <div>
+                    <Field component={'input'} type={'checkbox'} name={'rememberMe'}/> remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+        );
+    }
+;
+
+const LoginReduxForm = reduxForm<FormDataType>({
+    // a unique name for the form
+    form: 'login'
+})(LoginForm)
 
 export default Login;
--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -2,18 +2,19 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReduser from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
-import usersReducer, {
-
-} from "./users-reducer";
+import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form'
+
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReduser,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
