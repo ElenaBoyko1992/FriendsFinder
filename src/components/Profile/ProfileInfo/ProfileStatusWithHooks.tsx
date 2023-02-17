ceb3dfa75c4d2ad81f4b524dcfdf9d97fad4a952
ElenaBoyko1992 <Bionka@inbox.ru>
2023-02-17 21:40:07 +0300

    85 lesson done

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,18 +1,27 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from './ProfileInfo.module.css';
 
 
 const ProfileStatusWithHooks = (props: any) => {
 
     let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+
+        setStatus(props.status)
+    }, [props.status])
 
     const activateEditMode = () => {
         setEditMode(true)
     }
     const deactivateEditMode = () => {
         setEditMode(false)
+        props.updateStatus(status)
     }
-
+    const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value)
+    }
     return (
         <div>
             {!editMode &&
@@ -22,7 +31,8 @@ const ProfileStatusWithHooks = (props: any) => {
             {editMode &&
                 <div>
                     <input
-                        autoFocus={true} onBlur={deactivateEditMode}/>
+                        autoFocus={true} onBlur={deactivateEditMode}
+                        onChange={onStatusChange} value={status}/>
                 </div>}
         </div>
     )
