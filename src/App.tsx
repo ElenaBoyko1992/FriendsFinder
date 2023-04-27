49c187b8a921d397cae7b1d62ed51bc34992e1e9
ElenaBoyko1992 <Bionka@inbox.ru>
2023-04-27 17:49:41 +0300

    94 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersСontainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import {connect, Provider} from "react-redux";
@@ -12,6 +10,9 @@ import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import store, {ReduxStoreType} from "./redux/redux-store";
 import Preloader from "./components/common/Preloader/Preloader";
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component<AppPropsType> {
     componentDidMount() {
@@ -24,29 +25,29 @@ class App extends React.Component<AppPropsType> {
         }
 
         return (
-
             <div className={'app-wrapper'}>
                 <HeaderContainer/>
                 <Navbar/>
                 <div className={'app-wrapper-content'}>
+                    <Suspense fallback={<Preloader/>}>
 
-                    {/* <Route path={'/dialogs'} component={Dialogs}/> применять при передаче компоненты без пропсов
+                        {/* <Route path={'/dialogs'} component={Dialogs}/> применять при передаче компоненты без пропсов
                     <Route path={'/profile'} component={Profile}/> применять при передаче компоненты без пропсов*/}
-
-                    <Route path={'/dialogs'}
-                           render={() => <DialogsContainer/>}/> {/*применять при передаче компоненты c пропсами*/}
-                    <Route
-                        path={'/profile/:userId?'} //указываем, что в адресе после profile может также прийти userId.
-                        //"?" указывает на то, что параметр :userId является опциональным
-                        render={() => <ProfileContainer/>}/> {/*применять при передаче компоненты c пропсами*/}
-                    <Route path={'/users'}
-                           render={() => <UsersContainer/>}/>
-                    <Route path={'/login'}
-                           render={() => <LoginPage/>}/>
-
-                    {/*                    <Route path={'/news'} component={News}/>
+                        <Route path={'/dialogs'}
+                               render={() => <DialogsContainer/>}/> {/*применять при передаче компоненты c пропсами*/}
+                        <Route
+                            path={'/profile/:userId?'} //указываем, что в адресе после profile может также прийти userId.
+                            //"?" указывает на то, что параметр :userId является опциональным
+                            render={() => <ProfileContainer/>}/> {/*применять при передаче компоненты c пропсами*/}
+                        <Route path={'/users'}
+                               render={() => <UsersContainer/>}/>
+                        <Route path={'/login'}
+                               render={() => <LoginPage/>}/>
+                        {/*                    <Route path={'/news'} component={News}/>
                     <Route path={'/music'} component={Music}/>
                     <Route path={'/settings'} component={Settings}/>*/}
+
+                    </Suspense>
                 </div>
             </div>
 
